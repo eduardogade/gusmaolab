6d749d6046153a6cd5d247c52b747b84eae957ca
Eduardo Gade Gusmao <eggduzao@gmail.com>
2026-08-29 10:57:15 -0300

    add untracked files

--- __ideas3/heirarchical_clustering.r
+++
@@ -1,3 +0,0 @@
-set.seed(42)
-clusters <- hclust(dist(iris[, -5]))
-plot(clusters)
--- __snap1/mod.rs
+++
@@ -1,6 +0,0 @@
-mod adam;
-mod gradient_descent;
-mod momentum;
-
-pub use self::adam::Adam;
-pub use self::gradient_descent::gradient_descent;
